1b3898a37126fecab440fb686bf7839761e3d5ec
Goran Tenic <tenic.g@gmail.com>
2022-12-12 14:18:57 +0100

    chuck norris jokes

--- apps/klantbeeld/klantbeeld/welcome/welcome.component.ts
+++ apps/klantbeeld/klantbeeld/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Professional} from "../entities/Professional";
-import {ProfessionalService} from "../../src/app/services/professional.service";
+import { Component, OnInit } from '@angular/core';
+import { Professional } from '../entities/Professional';
+import { ProfessionalService } from '../../src/app/services/professional.service';
+import { HttpClient } from '@angular/common/http';
+import { ChuckJoke } from '../entities/ChuckJoke';
 
 @Component({
   selector: 'maffe-maandag-welcome',
@@ -8,20 +10,32 @@ import {ProfessionalService} from "../../src/app/services/professional.service";
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  professionals: Professional[] = [];
+  joke = '';
 
-  professionals : Professional[] = [];
-
-  constructor(private professionalService : ProfessionalService){
-
-  }
+  constructor(
+    private professionalService: ProfessionalService,
+    private client: HttpClient
+  ) {}
 
   voegProfessionalToe() {
-    return "";
+    return '';
   }
 
   ngOnInit(): void {
     this.professionalService.getAllProfessionals().subscribe((res) => {
       this.professionals = res;
-    })
+    });
+    this.getJoke();
+  }
+
+  getJoke() {
+    this.client
+      .get<ChuckJoke>('https://api.chucknorris.io/jokes/random')
+      .subscribe({
+        next: (res: ChuckJoke) => {
+          this.joke = res.value;
+        },
+      });
   }
 }
