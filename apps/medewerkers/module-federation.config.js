6b128236acec065917963571c1974b6c15200707
Eelco Duinsbergen <eduinsbergen@vxcompany.com>
2022-12-12 13:28:46 +0100

    add medewerkers app

---
+++ apps/medewerkers/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'medewerkers',
+  exposes: {
+    './Module': 'apps/medewerkers/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ apps/medewerkers/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maffe-maandag-medewerkers-entry',
+  template: `<maffe-maandag-nx-welcome></maffe-maandag-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
