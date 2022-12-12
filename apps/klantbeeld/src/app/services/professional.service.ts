4610c49da0afaed6b28be18524535833c0bc9eef
Jesse Kruisheer <70973567+JKruisheer@users.noreply.github.com>
2022-12-12 14:17:14 +0100

    Weer in main
    Again.....

--- apps/klantbeeld/src/app/services/professional.service.ts
+++ apps/klantbeeld/src/app/services/professional.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Professional} from "../../../klantbeeld/entities/Professional";
 import {Observable} from "rxjs";
+import {storeOutputsWatcherSubscription} from "nx/src/daemon/server/shutdown-utils";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,21 @@ export class ProfessionalService {
 
   getAllProfessionals() : Observable<Professional[]> {
     return this.httpClient.get<Professional[]>("https://api2.maffe-maandag.nl/profs")
+  }
+
+  addAProfessional(): Professional {
+    const value : Professional = {
+      id: Math.round(Math.random() * 1000000),
+      name: "Geen zin om dit te implementeren",
+      happinessindex: 1123
+    };
+    console.log(JSON.stringify(value))
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json'});
+    // const options = { headers: headers };
+    this.httpClient.post<Professional>("https://api2.maffe-maandag.nl/profs", JSON.stringify(value)).subscribe((res) => {
+      console.log(res);
+    })
+    return value;
   }
 }
